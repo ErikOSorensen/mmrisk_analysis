30fbb795458017c3547ffd3046f8e232876b207d
Erik Ø. Sørensen <erik.sorensen@nhh.no>
2025-05-23 10:53:14 +0200

    Added summaries (with Rhat).

--- main.R
+++ main.R
@@ -7,5 +7,6 @@ library(future)
 plan("multisession")
 # Run pipeline
 #tar_destroy(destroy="all")
-tar_make_future(c("fit_dl_now", "fit_dl_short", "fit_dl_long","fit_dl_never")", workers = 32, callr_function = NULL)
-tar_make(c("Traceplots","Results"))))
+tar_make_future(c("fit_dl_now", "fit_dl_short", "fit_dl_long","fit_dl_never"), workers = 32, callr_function = NULL)
+tar_make()
+
